6246a8edabd4674241a5d1330805eab7f062f825
st:prchal <steveruu@stvr.cz>
2024-04-18 19:45:04 +0200

    ono to dává smysl????

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
-const os = require('os');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,43 +1,58 @@
-import './index.css';
-import fs from 'fs';
+import "./index.css";
 
-const clipboard = require('electron').clipboard;
+const clipboard = require("electron").clipboard;
+const os = require("os");
+const child_process = require("child_process");
 
-const ytdlpurl = document.getElementById('ytdlp') as HTMLInputElement;
-const downloadurl = document.getElementById('downloadurl') as HTMLInputElement;
-const location = document.getElementById('location') as HTMLInputElement;
+const ytdlpurl = document.getElementById("ytdlp") as HTMLInputElement;
+const downloadurl = document.getElementById("downloadurl") as HTMLInputElement;
+const location = document.getElementById("location") as HTMLInputElement;
 
-const mp3btn = document.getElementById('mp3') as HTMLButtonElement;
-const mp4btn = document.getElementById('mp4') as HTMLButtonElement;
+const mp3btn = document.getElementById("mp3") as HTMLButtonElement;
+const mp4btn = document.getElementById("mp4") as HTMLButtonElement;
 
-function getYTDLP() : string {
-   // get ytdlp path - windows system32, apple inside of app, linux /usr/bin
-   return "";
+function getYTDLP(): string {
+  // get ytdlp path - windows system32, apple inside of app, linux /usr/bin
+  if (process.platform === "win32") {
+    return "C:\\Windows\\System32\\yt-dlp.exe";
+  } else if (process.platform === "darwin") {
+    return "/Applications/ytdlp-downloader/yt-dlp";
+  } else {
+    return "/usr/bin/yt-dlp";
+  }
+
+  return "";
 }
 
-function getDownloadUrl() : string {
-    if (clipboard.readText().includes('https://')) {    
-        return clipboard.readText();
-    } else {
-        return ""; // at to neni undefined
-    }
+function getDownloadUrl(): string {
+  if (clipboard.readText().includes("https://")) {
+    return clipboard.readText();
+  } else {
+    return ""; // at to neni undefined
+  }
 }
 
-function getDownloadDir() : string  {
-    // get users path and add downloads
-    return "";
+function getDownloadDir(): string {
+  // get users path and add downloads
+
+  if (process.platform === "win32") {
+    return `${os.homedir()}\\Downloads`;
+  } else {
+    return `${os.homedir()}/Downloads`;
+  }
 }
 
 ytdlpurl.value = getYTDLP();
 location.value = getDownloadDir();
 downloadurl.value = getDownloadUrl();
 
-mp3btn.addEventListener('click', () => {
+mp3btn.addEventListener("click", () => {
   const ytdlp = ytdlpurl.value;
   const url = downloadurl.value;
   const dir = location.value;
-  const command = `${ytdlp} -x --audio-format mp3 --audio-quality 0 --output "${dir}/%(title)s.%(ext)s" ${url}`;
-  fs.writeFileSync('command.bat', command);
-    require('child_process').exec('command.bat');
-
+  
+  child_process.exec(`${getYTDLP() }`, (error: any, stdout: any, stderr: any) => {
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 });
